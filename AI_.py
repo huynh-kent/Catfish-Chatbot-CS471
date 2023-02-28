b8ecc60b0366c54013d57d250a0ab58bc9ec5b5f
huynh-kent <huynhkentd@gmail.com>
2023-02-27 22:02:00 -0800

    increase mood change values

--- AI_.py
+++ AI_.py
@@ -17,9 +17,9 @@ class AI:
     
     #TODO: Fix algorithm to set the mood once we can determine the percentajes
     def setMood(self):
-        if self.mood_value > .2:
+        if self.mood_value > 1:
             self.mood = "happy"
-        elif self.mood_value < (-.2):
+        elif self.mood_value < (-1):
             self.mood = "mad"
         else:
             self.mood = "neutral"
